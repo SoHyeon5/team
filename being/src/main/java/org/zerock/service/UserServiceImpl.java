88293933923b6c67f80d58a13be294a3f1cc692d
SoHyeon5 <dksthgus6126@naver.com>
2021-07-13 17:05:55 +0900

    +++

--- being/src/main/java/org/zerock/service/UserServiceImpl.java
+++ being/src/main/java/org/zerock/service/UserServiceImpl.java
@@ -1,32 +1,35 @@
 package org.zerock.service;
 
 import java.util.Date;
-import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.domain.UserVO;
 import org.zerock.dto.LoginDTO;
-import org.zerock.persistence.UserDAO;
+import org.zerock.mapper.UserMapper;
 
 @Service
 public class UserServiceImpl implements UserService {
-	@Inject
-	private UserDAO dao;
+	@Autowired
+	private UserMapper userMapper;
 
 	@Override
+	@Transactional
 	public UserVO login(LoginDTO dto) throws Exception {
-		return dao.login(dto);
+		return userMapper.login(dto);
 	}
 
 	@Override
+	@Transactional
 	public void keepLogin(String email, String sessionId, Date next) throws Exception {
-
-		dao.keepLogin(email, sessionId, next);
+		userMapper.keepLogin(email, sessionId, next);
 
 	}
 
 	@Override
+	@Transactional
 	public UserVO checkLoginBefore(String value) {
-
-		return dao.checkUserWithSessionKey(value);
+		return userMapper.checkUserWithSessionKey(value);
 	}
 }
