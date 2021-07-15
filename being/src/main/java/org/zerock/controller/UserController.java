d2706e89373421f913f32c59b506eb4f61ac72a7
Serei63 <runesiel@gmail.com>
2021-07-15 09:58:38 +0900

    logout

--- being/src/main/java/org/zerock/controller/UserController.java
+++ being/src/main/java/org/zerock/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.WebUtils;
-import org.zerock.domain.BoardVO;
+//import org.zerock.domain.BoardVO;
 import org.zerock.domain.UserVO;
 import org.zerock.dto.LoginDTO;
 import org.zerock.service.UserService;
@@ -25,60 +25,63 @@ import org.zerock.service.UserService;
 @Controller
 @RequestMapping("/user/*")
 public class UserController {
-	
-//	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-	
-	@Inject
-	private UserService service;
 
-	
-//	@RequestMapping(value = "/loginForm")
-//	public String userLogin(Model model, LoginDTO dto) throws Exception {
-//		logger.info("// /user/loginForm");
-//
-//		UserVO loginForm = userService.login(dto);
-//
-//		logger.info("// loginForm.toString()=" + loginForm.toString());
-//
-//		model.addAttribute("loginForm", loginForm);
-//		
-//		return "/user/loginForm";
-//		
-//	}
-	
-	@RequestMapping(value = "/loginForm", method = RequestMethod.GET)
-	public void loginGET(@ModelAttribute("dto") LoginDTO dto) {
-	}
-	
-	@RequestMapping(value = "/loginPost", method = RequestMethod.POST)
-	public void loginPOST(LoginDTO dto, HttpSession session, Model model) throws Exception {
-//		System.out.println("/user/loginPost 실행 ....");
-		UserVO vo = service.login(dto);
-		if (vo == null) {
-			return;
-		}
-		model.addAttribute("userVO", vo);
-		if (dto.isUseCookie()) {
-			int amount = 60 * 60 * 24 * 7;
-			Date sessionLimit = new Date(System.currentTimeMillis() + (1000 * amount));
-			service.keepLogin(vo.getEmail(), session.getId(), sessionLimit);
-		}
-	}
+//   private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 
-	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public void logout(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
-		Object obj = session.getAttribute("login");
-		if (obj != null) {
-			UserVO vo = (UserVO) obj;
-			session.removeAttribute("login");
-			session.invalidate();
-			Cookie loginCookie = WebUtils.getCookie(request, "loginCookie");
-			if (loginCookie != null) {
-				loginCookie.setPath("/");
-				loginCookie.setMaxAge(0);
-				response.addCookie(loginCookie);
-				service.keepLogin(vo.getEmail(), session.getId(), new Date());
-			}
-		}
-	}
+   @Inject
+   private UserService service;
+
+//   @RequestMapping(value = "/loginForm")
+//   public String userLogin(Model model, LoginDTO dto) throws Exception {
+//      logger.info("// /user/loginForm");
+//
+//      UserVO loginForm = userService.login(dto);
+//
+//      logger.info("// loginForm.toString()=" + loginForm.toString());
+//
+//      model.addAttribute("loginForm", loginForm);
+//      
+//      return "/user/loginForm";
+//      
+//   }
+
+   @RequestMapping(value = "/loginForm", method = RequestMethod.GET)
+   public void loginGET(@ModelAttribute("dto") LoginDTO dto) {
+   }
+
+   @RequestMapping(value = "/loginPost", method = RequestMethod.POST)
+   public void loginPOST(LoginDTO dto, HttpSession session, Model model) throws Exception {
+      System.out.println("/user/loginPost 실행 ....");
+      UserVO vo = service.login(dto);
+      // System.out.println("*** UserVO.name="+vo.getName());
+      if (vo == null) {
+         return;
+      }
+      model.addAttribute("userVO", vo);
+      if (dto.isUseCookie()) {
+         int amount = 60 * 60 * 24 * 7;
+         Date sessionLimit = new Date(System.currentTimeMillis() + (1000 * amount));
+         service.keepLogin(vo.getEmail(), session.getId(), sessionLimit);
+      }
+   }
+
+   @RequestMapping(value = "/logout", method = RequestMethod.GET)
+   public String logout(HttpServletRequest request, HttpServletResponse response, HttpSession session)
+         throws Exception {
+      Object obj = session.getAttribute("login");
+      if (obj != null) {
+         UserVO vo = (UserVO) obj;
+         session.removeAttribute("login");
+         session.invalidate();
+         Cookie loginCookie = WebUtils.getCookie(request, "loginCookie");
+         if (loginCookie != null) {
+            loginCookie.setPath("/");
+            loginCookie.setMaxAge(0);
+            response.addCookie(loginCookie);
+            service.keepLogin(vo.getEmail(), session.getId(), new Date());
+         }
+      }
+
+      return "user/logout";
+   }
 }
